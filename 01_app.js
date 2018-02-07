ab4e536306b16bd0dd1ede7677dd3e5fe0ad7b9c
Samuel Poisson <spoisson@hotmail.ca>
2018-02-07 10:36:11 -0500

    Ajout des champs Téléphone et Courriel dans le formulaire

---
+++ 01_app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+let compteur=0;
+console.log('initialise compteur = ' + compteur);
+
+app.get('/', (req, res) => {
+   console.log('incrémente compteur dans route = ' + compteur++);
+   res.send('<h1>Vive Express</h1>');
+
+})
+
+const server = app.listen(8081, () => {
+   let host = server.address().address
+   let port = server.address().port
+   
+   console.log("Le serveur Express écoute http://%s:%s", host, port)
+})
