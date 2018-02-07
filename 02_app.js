2f5d3d4b5002d7b888b80b8eda83bfee87c753f6
Samuel Poisson <spoisson@hotmail.ca>
2018-02-07 12:01:56 -0500

    Fonction permettant d'afficher les informations des membres dans un tableau HTML

--- 02_app.js
+++ 02_app.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+
+const transforme_en_tableau = (listeJSON) => {
+
+  let trace = '<table>';
+
+  for (let elm of listeJSON){
+
+    for (let p in elm) { 
+
+      trace += '<tr><th>' + p + '</th>';
+      trace += '<th>' + elm[p] + '</th></tr>'; 
+
+    } 
+
+      trace += '<tr><th>&nbsp</th></tr>';
+
+  }
+
+  trace += '</table>';
+  return trace;
+  //console.log(trace);
+
+}
+
 ////////////////////////////////////////////////// Route /html/01_form.htm
 app.get('/formulaire', function (req, res) {
  console.log(__dirname);
@@ -24,11 +48,53 @@ console.log('la route /traiter_get')
  telephone:req.query.telephone,
  courriel: req.query.courriel
  };
+ 
+//console.log(reponse);
+//res.end(JSON.stringify(reponse));
+
+let fs = require('fs');
+  fs.appendFile('public/data/membres.txt', ',' + JSON.stringify(reponse), (err) => {
+  if (err) throw err;
+     console.log('Sauvegardé');
+});
+
+/*
+let fs = require('fs');
+fs.readFile('public/data/membres.txt', 'utf8', (err, data) => {
+ if (err) throw err;
+ //let obj = JSON.parse(data);
+ console.log(JSON.parse(data));
+
+
+});
+*/
+
+
+
+
 console.log(reponse);
  res.end(JSON.stringify(reponse));
 })
 /////////////////////////////////////// Route : membres
-app.get('membres', (req,res) => {
+app.get('/membres', (req,res) => {
+/*
+    res.sendFile( __dirname + "/public/data/" + "membres.txt" );
+    console.log(res);
+
+*/
+    let fs = require('fs');
+    fs.readFile('public/data/membres.txt', 'utf8', (err, data) => {
+
+     if (err) throw err;
+     let listeMembres = JSON.parse('[' + data + ']');
+
+     console.log(listeMembres);
+
+     res.end(transforme_en_tableau(listeMembres));
+      
+      //res.send(data);
+
+    });
 
 
 
